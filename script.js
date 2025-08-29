15c67902af5e78d5a1a092fae809ec5fa6f41719
Md Bayejid <mdbayejid1004@gmail.com>
2025-08-29 15:57:04 +0600

    card functionality added

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-document.getElementById('button1').addEventListener(
-    'click', function(){
-        alert('Copy Done')
-    }
-)
+
+
+// heart click functionality
+
+function updateHeartNumber(id) {
+
+    document.getElementById(id).addEventListener(
+        'click', function () {
+            const heartNumber = parseInt(document.getElementById('heart-number').innerText);
+            console.log(heartNumber);
+            const newHeartNumber = heartNumber + 1;
+            document.getElementById('heart-number').innerText = newHeartNumber;
+        }
+    )
+}
+
+updateHeartNumber('heart-click-1');
+updateHeartNumber('heart-click-2');
+updateHeartNumber('heart-click-3');
+updateHeartNumber('heart-click-4');
+updateHeartNumber('heart-click-5');
+updateHeartNumber('heart-click-6');
+updateHeartNumber('heart-click-7');
+updateHeartNumber("heart-click-9");
+updateHeartNumber("heart-click-8");
+
+
+
